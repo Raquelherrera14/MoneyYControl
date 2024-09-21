bd24ef8cf25ed8aecb4cf4fa2af1a1f9a4f08be1
sonia <soniatomayconza01@gmail.com>
2024-09-21 14:41:09 -0500

    v2

--- src/main/java/com/upc/tp_moneycontrol/controller/GarantiasController.java
+++ src/main/java/com/upc/tp_moneycontrol/controller/GarantiasController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200", "http://3.144.144.160/"})
-
+//h
 @RestController
 @RequestMapping("/api/Garantias")
 public class GarantiasController {
